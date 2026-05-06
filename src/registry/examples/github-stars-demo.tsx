adeb6936cecb199da9cb9c5a3378d3d7510f6366
Nguyễn Chánh Đại <dai@chanhdai.com>
2026-05-06 22:35:34 +0700

    fix: update star count for chanhdai.com in user profile

--- src/registry/examples/github-stars-demo.tsx
+++ src/registry/examples/github-stars-demo.tsx
@@ -1,5 +1,5 @@
 import { GitHubStars } from "@/registry/components/github-stars"
 
 export default function GitHubStarsDemo() {
-  return <GitHubStars repo="ncdai/chanhdai.com" stargazersCount={1560} />
+  return <GitHubStars repo="ncdai/chanhdai.com" stargazersCount={1800} />
 }
