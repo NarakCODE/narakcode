67eebdb6638f4aad591cade1de647aa82523b0e4
Nguyễn Chánh Đại <dai@chanhdai.com>
2026-06-12 02:38:24 +0700

    docs: update stargazersCount from 1800 to 2000 in github-stars examples

--- src/registry/examples/github-stars-demo.tsx
+++ src/registry/examples/github-stars-demo.tsx
@@ -1,5 +1,5 @@
 import { GitHubStars } from "@/registry/transformed/components/github-stars"
 
 export default function GitHubStarsDemo() {
-  return <GitHubStars repo="ncdai/chanhdai.com" stargazersCount={1800} />
+  return <GitHubStars repo="ncdai/chanhdai.com" stargazersCount={2000} />
 }
